c670984914b2b1eff313e91f480e1fc1bf474422
André <andre.koeniger1997@gmail.com>
2026-06-24 20:25:19 +0200

    Update config values, remove db

--- backend/travel-info/config/config.go
+++ backend/travel-info/config/config.go
@@ -11,12 +11,11 @@ type Config struct {
 	OTELCollectorEndpoint string   `envconfig:"OTEL_COLLECTOR_ENDPOINT" default:""`
 	AuthServiceURL        string   `envconfig:"AUTH_SERVICE_URL"`
 	// Travel warning
-	TravelWarningUrl string `envconfig:"TRAVEL_WARNING_URL"`
+	TravelWarningUrl string `envconfig:"TRAVEL_API_URL"`
 	// Weather
 	WeatherAPIUrl        string `envconfig:"WEATHER_API_URL"`
 	WeatherCacheTTLHours int    `envconfig:"FORECAST_DAYS" default:"3"`
 	WeatherForecastDays  int    `envconfig:"CACHE_TTL_HOURS" default:"24"`
-	LocationsDBURL       string `envconfig:"LOCATIONS_DB_URL"`
 }
 
 func LoadConfig() (*Config, error) {
